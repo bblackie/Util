730b52be37a61c44e156a76a379b730a012812cd
bblackie <learndigitech2day@gmail.com>
2025-08-13 14:33:58 +1200

    Work laptop updates

--- create-folders.py
+++ create-folders.py
@@ -1,8 +1,8 @@
 import os
 
 
-list = 'names-Y11.txt'
-folder_root = 'C:\\Users\\brian\\OneDrive - Trinity Schools\\Classes\\11DGT\\Assessments\\AS92004(1.1) Programming\\Student work\\'
+list = 'names-y12-initials.txt'
+folder_root = 'C:\\Users\\Brian.Blackie\\OneDrive - Trinity Schools\\Classes\\12DGT\\Assessments\\AS91893(2.4) Website\\Student work\\'
 folder_root2 = 'C:\\Users\\brian.blackie\\OneDrive - Trinity Schools\\Classes\\13DGT\\Assessments\\3.7 Computer Program\\Student work\\'
 
 
