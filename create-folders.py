6c21e8658de2fbc29d26f9bd7fddf74193055d18
bblackie <brian.blackie@trinityschools.nz>
2026-02-09 18:00:08 +1300

    publish

--- create-folders.py
+++ create-folders.py
@@ -3,8 +3,9 @@ import os
 
 list = 'initials-Y12.txt'
 folder_root = 'C:\\Repos\\12DGT\\91893-web\\'
-folder_root2 = 'C:\\Users\\brian\\OneDrive - Trinity Schools\\Classes\\12DGT\\Assessments\\AS91896(2.7) Computer Program\\Student work\\'
 
+list2 = 'subjects.txt'
+folder_root2 = 'C:\\Users\\brian\\OneDrive - Trinity Schools\\NCEA\\L3\\achievements\\AS91908 (3.9) - CS Area\\Past exams\\by topic\\'
 
 #folder_root = 'C:\\Users\\brian.blackie\\OneDrive - Trinity Schools\\NCEA\\Exams - DCATs\\DCATs\\L3\\91909\\Student submissions\\'
 #folder_root = 'D:\\src\\11DGT\\'
@@ -14,12 +15,12 @@ folder_root2 = 'C:\\Users\\brian\\OneDrive - Trinity Schools\\Classes\\12DGT\\As
 
 
 
-
-with open(f'data/classes/{list}') as x:
+#with open(f'data/classes/{list}') as x:
+with open(f'data/categories/{list2}') as x:
     for line in x:
-        line = line.strip()
-        if not os.path.exists(folder_root + str(line)):
-            os.mkdir(folder_root + str(line))
+        line = line.strip() 
+        if not os.path.exists(folder_root2 + str(line)):
+            os.mkdir(folder_root2 + str(line))
 
 
 print('Mission complete.')
